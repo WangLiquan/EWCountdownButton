d0244eac710446717eac29c513f3f74f3751a83a
Ethan <wang197200@163.com>
2019-04-04 16:45:15 +0800

    功能实现

--- EWCountdownButton/ViewController.swift
+++ EWCountdownButton/ViewController.swift
@@ -10,11 +10,21 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    let button: UIButton = {
+        let button = UIButton(frame: CGRect(x: 100, y: 100, width: 200, height: 200))
+        button.backgroundColor = UIColor.green
+        button.addTarget(self, action: #selector(onClickButton(_:)), for: .touchUpInside)
+        return button
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.view.addSubview(button)
         // Do any additional setup after loading the view.
     }
 
-
+    @objc private func onClickButton(_ sender: UIButton) {
+        sender.countDown(count: 60)
+    }
 }
 
